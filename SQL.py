cff22bad5def12961852b8f85567df9998631068
djjohnsongeek <danieleejohnson@gmail.com>
2019-06-15 11:24:09 -0400

    added back up and restore funcions

--- SQL.py
+++ SQL.py
@@ -1,10 +1,12 @@
 import sqlite3
 from sqlite3 import Error
 
-def db_connect(URL):
+BACKUP = "C:\\Users\\Johnson\\Documents\\Projects\\achieve\\backup.db"
+
+def db_connect(db_path):
     """ A simple fuction to make connection to SQLite databases easier """
     try:
-        conn = sqlite3.connect(URL)
+        conn = sqlite3.connect(db_path)
         conn.row_factory = sqlite3.Row
         db = conn.cursor()
     except Error as e:
@@ -12,3 +14,31 @@ def db_connect(URL):
         conn.close()
     else:  
         return db, conn
+
+def db_backup(existing_db):
+    try:
+        conn = sqlite3.connect(existing_db)
+        bck = sqlite3.connect(BACKUP)
+    except Error as e:
+        print(e)
+        conn.close()
+
+    with bck:
+        conn.backup(bck)
+
+    conn.close()
+    bck.close()
+
+def db_restore(existing_db):
+    try:
+        conn = sqlite3.connect(existing_db)
+        bck = sqlite3.connect(BACKUP)
+    except Error as e:
+        print(e)
+        conn.close()
+        
+    with conn:
+        bck.backup(conn)
+
+    conn.close()
+    bck.close()
